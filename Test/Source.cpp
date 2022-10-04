d40cb1b3190b60d90ce8d47dce71807257dd9ea8
Equinox <61376753+Ephemerish@users.noreply.github.com>
2022-10-04 18:02:28 +0800

    chore: cleaning needless files

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,20 +1,55 @@
 #include <iostream>
 #include <string>
+#include <SFML/Graphics.hpp>
 
-int m_rightScore = 0;
-int m_leftScore = 0;
+void main(int argc, char** argv[]) {
+	sf::RenderWindow window(sf::VideoMode(800, 600),
+		"Bouncing mushroom.");
+	sf::Texture mushroomTexture;
+	mushroomTexture.loadFromFile("assets/Mushroom2.png");
+	sf::Sprite mushroom(mushroomTexture);
+	//mushroom.setScale(0.1f,0.1f);
+	sf::Vector2u size = mushroomTexture.getSize();
+	//size.x /= 10.f;
+	//size.y /= 10.f;
+	mushroom.setOrigin(size.x / 2, size.y / 2);
+	sf::Vector2f increment(0.04f, 0.04f);
 
-std::string getStanding()
-{
-	std::string standing;
-	standing = std::to_string(m_leftScore) + " : " + std::to_string(m_rightScore);
-	return standing;
-}
+	sf::Clock clock;
 
-int main()
-{
-	m_rightScore++;
-	std::cout << getStanding();
-	std::cin.get();
-	return 0;
+	while (window.isOpen())
+	{
+		
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		
+		if ((mushroom.getPosition().x + (size.x / 2) >
+			window.getSize().x && increment.x > 0) ||
+			(mushroom.getPosition().x - (size.x / 2) < 0 &&
+				increment.x < 0))
+		{
+			// Reverse the direction on X axis.
+			increment.x = -increment.x;
+		}
+		if ((mushroom.getPosition().y + (size.y / 2) >
+			window.getSize().y && increment.y > 0) ||
+			(mushroom.getPosition().y - (size.y / 2) < 0 &&
+				increment.y < 0))
+		{
+			// Reverse the direction on Y axis.
+			increment.y = -increment.y;
+		}
+
+		double deltaTime = clock.restart().asSeconds();
+
+		mushroom.setPosition(mushroom.getPosition() + (increment));
+		window.clear(sf::Color(16, 16, 16, 255)); // Dark gray.
+		window.draw(mushroom); // Drawing our sprite.
+		window.display();
+	}
 }
